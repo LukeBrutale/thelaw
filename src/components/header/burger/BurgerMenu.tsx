8f9e9215df3134616b5b8dcd21b6908f5a1a66d3
LukeBrutale <yuriybema@gmail.com>
2024-10-02 17:40:04 +0300

    commit

--- src/components/header/burger/BurgerMenu.tsx
+++ src/components/header/burger/BurgerMenu.tsx
@@ -26,14 +26,16 @@ export default function BurgerMenu() {
 
       {/* Бургер меню */}
       <div className={`fixed top-0 right-[-10px] h-[100vh] w-[75vw] bg-background-100 shadow-lg transition-transform duration-500 ease-in-out ${isOpen ? "translate-x-0" : "translate-x-full"} md:hidden z-50`}>
-        <div className=" bg-background-100">
-          <div className="flex justify-end p-5">
-            <CloseButton />
+        <div className="h-screen flex flex-col justify-between">
+          <div className=" bg-background-100">
+            <div className="flex justify-end p-5">
+              <CloseButton />
+            </div>
+            <Navigation />
           </div>
-          <Navigation />
-        </div>
-        <div className="flex justify-center mt-10">
-          <SocialMedia />
+          <div className="mx-auto mb-5">
+            <SocialMedia />
+          </div>
         </div>
       </div>
     </div>
